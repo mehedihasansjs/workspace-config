30e1e8fc73effb6bb0b29757d6a49b4dfe23046f
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2024-06-22 13:07:29 +0600

    added Amazon Ember and Inter fonts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# install fonts
+echo "🏃‍♂️‍➡️ installing agave fonts..."
+cp -r ./fonts/agave/* ~/Library/Fonts/
+echo "✅ done"
+
+echo "🏃‍♂️‍➡️ installing amazon ember fonts..."
+cp -r ./fonts/amazon-ember/* ~/Library/Fonts/
+echo "✅ done"
+
+echo "🏃‍♂️‍➡️ installing inter fonts..."
+cp -r ./fonts/inter/* ~/Library/Fonts/
+echo "✅ done"
